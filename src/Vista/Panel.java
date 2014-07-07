5b073698dccf00ffe2e213717c5ce8505d8d6b00
PC <PC@VAIOsony>
2014-07-06 19:33:58 -0500

    Puesta de imagen de Ciudad

---
+++ src/Vista/Panel.java
@@ -0,0 +1,39 @@
+package Vista;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class Panel extends JPanel {
+
+    private Image imagen;
+
+    public Panel() {
+    }
+
+    public void setImagen(String nombreImagen) {
+        if (nombreImagen != null) {
+            imagen = new ImageIcon(getClass().getResource(nombreImagen)).getImage();
+        } else {
+            imagen = null;
+        }
+
+        repaint();
+        
+    }
+   
+    @Override
+    public void paint(Graphics g) {
+        if (imagen != null) {
+            g.drawImage(imagen, 165, 0, 915, 715, this);
+
+            setOpaque(false);
+        } else {
+            setOpaque(true);
+        }
+
+        super.paint(g);
+        
+    }
+}
