927ff7a1064887ecd1bd0fd63a4389aa9b6a0c38
cristian1853 <cristian@trescafe.com>
2014-07-13 11:01:36 -0500

    ahora si

--- src/Grafo/Nodo.java
+++ src/Grafo/Nodo.java
@@ -13,10 +13,91 @@ import java.awt.Rectangle;
  * @author CRISTIAN
  */
 public class Nodo {
-    Object huesped;//Tipo de dato que va a alojar: Empresa/C.DistribuciÃ³n/C.Abastecimiento.
-    int x;
-    int y;
-    int tamano;
-    Rectangle area;//Ã�rea que cubre el nodo
+    private Object huesped;//Tipo de dato que va a alojar: Empresa/C.DistribuciÃ³n/C.Abastecimiento.
+    private int x;
+    private int y;
+    private int tamano;
+    private Rectangle area;//Ã�rea que cubre el nodo
+
+    public Nodo(int x, int y) {
+        this.huesped = null;
+        this.x = x;
+        this.y = y;
+        this.tamano = 32;
+        this.area = new Rectangle(x, y, this.getTamano(), this.getTamano());
+    }
+
+    /**
+     * @return the huesped
+     */
+    public Object getHuesped() {
+        return huesped;
+    }
+
+    /**
+     * @param huesped the huesped to set
+     */
+    public void setHuesped(Object huesped) {
+        this.huesped = huesped;
+    }
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * @return the tamano
+     */
+    public int getTamano() {
+        return tamano;
+    }
+
+    /**
+     * @param tamano the tamano to set
+     */
+    public void setTamano(int tamano) {
+        this.tamano = tamano;
+    }
+
+    /**
+     * @return the area
+     */
+    public Rectangle getArea() {
+        return area;
+    }
+
+    /**
+     * @param area the area to set
+     */
+    public void setArea(Rectangle area) {
+        this.area = area;
+    }
+
+    
+    
     
 }
